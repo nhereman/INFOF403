c556e8d6b3c246d417c65d35df3d52df23ecc47e
Hereman Nicolas <nhereman@ulb.ac.be>
2015-10-17 15:12:09 +0200

    Add identifiers part

--- Part 1/Main.java
+++ Part 1/Main.java
@@ -1,6 +1,8 @@
 import java.io.FileReader;
 import java.io.File;
 import java.io.IOException;
+import java.util.TreeMap;
+import java.util.Map;
 
 public class Main {
 
@@ -13,14 +15,27 @@ public class Main {
 				FileReader reader = new FileReader(f);
 				LexicalAnalyzer analyzer = new LexicalAnalyzer(reader);
 
+				TreeMap<String, Integer> identifiers = new TreeMap<String, Integer>();
+
 				Symbol symbol = null;
 
 				do {
 					symbol = analyzer.nextToken();
 					if ( symbol != null && symbol.getType() != LexicalUnit.END_OF_STREAM) {
 						System.out.println(symbol.toString());
+						if ( symbol.getType() == LexicalUnit.VARNAME && !identifiers.containsKey((String) symbol.getValue()) ) {
+							identifiers.put((String) symbol.getValue(), symbol.getLine());
+						}
 					}
 				} while(symbol == null || symbol.getType() != LexicalUnit.END_OF_STREAM);
+
+				System.out.println("Identifiers");
+
+				for (Map.Entry<String,Integer> entry : identifiers.entrySet()) {
+					System.out.print(entry.getKey()+" ");
+					System.out.println(entry.getValue());
+				}
+
 			} catch(IOException exception) {
 				 System.out.println ("Erreur during read : " + exception.getMessage());
 			}
