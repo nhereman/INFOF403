8fa42f2c29162797320455b9f6b7807ab7837789
Hereman Nicolas <nhereman@ulb.ac.be>
2015-11-16 17:46:38 +0100

    Adding Part2 + Grammar modification

---
+++ Part 2/more/Main.java
@@ -0,0 +1,48 @@
+import java.io.FileReader;
+import java.io.File;
+import java.io.IOException;
+import java.util.TreeMap;
+import java.util.Map;
+import java.util.regex.PatternSyntaxException;
+
+public class Main {
+
+	public static void main (String[] args){
+		if ( args.length < 1 ) {
+			System.out.println("Error: Not enough argument");
+		} else {
+			File f = new File(args[0]);
+			try {
+				FileReader reader = new FileReader(f);
+				LexicalAnalyzer analyzer = new LexicalAnalyzer(reader);
+
+				TreeMap<String, Integer> identifiers = new TreeMap<String, Integer>();
+
+				Symbol symbol = null;
+
+				do {
+					symbol = analyzer.nextToken();
+					if ( symbol != null && symbol.getType() != LexicalUnit.END_OF_STREAM) {
+						System.out.println(symbol.toString());
+						if ( symbol.getType() == LexicalUnit.VARNAME && !identifiers.containsKey((String) symbol.getValue()) ) {
+							identifiers.put((String) symbol.getValue(), symbol.getLine());
+						}
+					}
+				} while(symbol == null || symbol.getType() != LexicalUnit.END_OF_STREAM);
+
+				System.out.println("Identifiers");
+
+				for (Map.Entry<String,Integer> entry : identifiers.entrySet()) {
+					System.out.print(entry.getKey()+" ");
+					System.out.println(entry.getValue());
+				}
+
+			} catch(IOException exception) {
+				System.out.println ("Error during read : " + exception.getMessage());
+			} catch(PatternSyntaxException e) {
+				System.out.println ("Parse Error : " + e.getDescription() );
+			}
+		}
+	}
+
+}
